97eff789453b7170817d31166793f59591b743b2
BenjaminBaredes <bbaredes@gmail.com>
2025-10-13 10:27:55 -0300

    soqutic back

--- back/index.js
+++ back/index.js
@@ -1,22 +1,25 @@
+import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 import fs from "fs";
 let data = fs.readFileSync("usuarios.json", "utf-8");
 
 
-function registrar(nombre){
-    let nombres = JSON.parse(data);
-    nombres.push(nombre);
-    let nuevoJson = JSON.stringify(nombres, null, 2);
+function registrar(nombre, contraseña){
+    let nombre = JSON.parse(data);
+    let contraseña = JSON.parse(data);
+    let usuario = (nombre, contraseña)
+    usuario.push(usuario);
+    let nuevoJson = JSON.stringify(usuario, null, 2);
     fs.writeFileSync("usuarios.json", nuevoJson);
     console.log("Nombre agregado con éxito!");
 }
 
-function login (nombre){
+function login (nombre, contraseña){
 
     let correcto = false;
     let usuarios = JSON.parse(data)
 
     for (var i = 0; i < usuarios.length; i++) {
-        if (nombre == usuarios[i]) {
+        if (nombre && contraseña == usuarios[i]) {
             correcto = true;
         }
     }
