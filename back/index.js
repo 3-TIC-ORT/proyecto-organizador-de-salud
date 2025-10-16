cbf13a59c78fb9bd1df5d22f99316bdfb046b8ea
BenjaminBaredes <bbaredes@gmail.com>
2025-10-16 11:58:54 -0300

    inicio de sesión sin coneexión con front

--- back/index.js
+++ back/index.js
@@ -1,26 +1,29 @@
-import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 import fs from "fs";
+
+
 let data = fs.readFileSync("usuarios.json", "utf-8");
 
 
-function registrar(nombre, contraseña){
-    let nombre = JSON.parse(data);
-    let contraseña = JSON.parse(data);
-    let usuario = (nombre, contraseña)
-    usuario.push(usuario);
-    let nuevoJson = JSON.stringify(usuario, null, 2);
+function registrarse(nombre, contraseña, mail, fecha, perfil, credencial){
+    let usuarios = JSON.parse(data);
+    usuarios.push({"nombre": nombre, "contra": contraseña,"mail": mail, "nacimiento": fecha, "perfil": perfil, "credencial": credencial});
+    let nuevoJson = JSON.stringify(usuarios, null, 2);
     fs.writeFileSync("usuarios.json", nuevoJson);
     console.log("Nombre agregado con éxito!");
 }
 
-function login (nombre, contraseña){
+function iniciosesión (nombre, contraseña, mail, fecha, perfil, credencial){
 
     let correcto = false;
     let usuarios = JSON.parse(data)
 
     for (var i = 0; i < usuarios.length; i++) {
-        if (nombre && contraseña == usuarios[i]) {
+        if (nombre == usuarios[i].nombre && contraseña == usuarios[i].contra && mail == usuarios[i].mail) {
             correcto = true;
         }
     }
 }
+
+registrarse("Luu", "123","luu", "02/04/97", "medico", "1235465");
+//nombre y apellido, mail, contraseña, fecha de nacimiento, médico/paciente, credencial/obra social }
+//iniciosesión nombre y apellido, mail, contraseña
