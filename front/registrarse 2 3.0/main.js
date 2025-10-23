1d2f8c41874e7d0ebda5e1e3b8cdf9c5c6c860a7
lunaezer <lunaezer22@gmail.com>
2025-10-23 09:00:37 -0300

    registrarse 2

--- front/registrarse 2 3.0/main.js
+++ front/registrarse 2 3.0/main.js
@@ -2,8 +2,25 @@
 
 connect2Server();
 
+function enviar(){
 
 let nombre = localStorage["nombre"]
+let nacimiento = document.getElementById("nacimiento").value;
+let rol = document.getElementById("rol").value;
+let matricula = document.getElementById("matricula").value;
+let datos = {"nombre":nombre, "nacimiento": nacimiento, "perfil": rol, "matricula": matricula};
 
-postEvent("actualizazr", datos);
+postEvent("actualizazr", datos, (respuesta) =>{
+    console.log(respuesta);
+    if(respuesta.error){
+        alert("Error");
+    }
+    else {
+        alert("Su usuario ha sido creado con exito!");
+        window.location.href = "../home opciones 4.0/index.html";
+    }
+  });
+}
 
+let boton = document.getElementById("bot-siguiente");
+boton.addEventListener("click", enviar);
