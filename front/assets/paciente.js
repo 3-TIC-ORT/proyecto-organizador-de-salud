d04acc94ce6a3226218b472d60267f3b4befeaed
lunaezer <lunaezer22@gmail.com>
2025-10-26 17:37:17 -0300

    vincule

---
+++ front/assets/paciente.js
@@ -0,0 +1,20 @@
+let home = document.getElementById("home");
+home.addEventListener("click", () => {
+    window.location.href = "../home opciones 4.0/index.html";
+});
+ let historial = document.getElementById("historial");
+historial.addEventListener("click", () => {
+    window.location.href = "../historial/index.html"
+});
+let calendario = document.getElementById("calendario");
+calendario.addEventListener("click", () => {
+    window.location.href = "../prueba2 cal/index.html"
+});
+let grupofam = document.getElementById("grupofam");
+grupofam.addEventListener("click", () => {
+    window.location.href = "../grupo familiar/index.html"
+});
+let mapa = document.getElementById("mapa");
+mapa.addEventListener("click", () => {
+    window.location.href = "../mapa 2.0/index.html"
+});
