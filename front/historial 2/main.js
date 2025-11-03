d88d7feb9bd27d0110ae7a4c9ea5ad5292caa93a
santiwinny <santiwinny@gmail.com>
2025-11-03 15:30:37 -0300

    historial 2

--- front/historial 2/main.js
+++ front/historial 2/main.js
@@ -1,3 +1,12 @@
 function redirigir() {
     window.location.href = "https://forms.gle/99QYz9jF14Ys6Shx6";
   }
+
+  let nombreTitulo = document.getElementById("nombre");
+
+
+
+function asignarNombre(usuario){
+    nombreTitulo.textContent = localStorage["usuario"];
+}
+asignarNombre();
