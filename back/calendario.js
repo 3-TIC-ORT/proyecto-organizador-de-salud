d53453d02dfdaa16fc771740fa0661ac1ccec6df
BenjaminBaredes <bbaredes@gmail.com>
2025-11-10 15:53:46 -0300

    .

--- back/calendario.js
+++ back/calendario.js
@@ -3,14 +3,17 @@ import fs from "fs";
 export function calendario(data){
     let calendario = JSON.parse(fs.readFileSync("calendario.json", "utf-8"));
 
-for (let i = 0; i < calendario.length; i++){
-
-        if (calendario[i].mail == data.mail){
-            calendario[i].mail[data.fecha].push(data.texto);
+    for (let i = 0; i < calendario.length; i++){
+        if (calendario[i].mail != data.mail) {
+           data.mail[data.fecha].push(data.texto);
+           return { "msg": true, "case": "1" };  
+        }
+        else if (calendario[i].mail == data.mail){
+        calendario[i].mail[data.fecha].push(data.texto);
+        return { "msg": true, "case": "2"};
         }
     }
-    fs.writeFileSync("calendario.json", JSON.stringify(calendario, null, 2));
-    return { msg: true };
+    fs.writeFileSync("calendario.json", JSON.stringify(calendario, null, 2));    
 }
 
 export function cargarEventos(events){
